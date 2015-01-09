8f2f21e9393bb18da848116a51576fc6df79a9e7
Jani Turunen <turunen.jani@gmail.com>
2015-01-09 21:08:22 +0200

    adding support for german shipping_addresses

--- lib/klarna/checkout/address.rb
+++ lib/klarna/checkout/address.rb
@@ -1,8 +1,8 @@
 module Klarna
   module Checkout
     class Address < Resource
-      attr_accessor :given_name, :family_name, :care_of, :street_address,
-                    :postal_code, :city, :country, :email, :phone
+      attr_accessor :given_name, :family_name, :care_of, :street_address, :street_name, :street_number,
+                    :title, :postal_code, :city, :country, :email, :phone
 
       def as_json
         json_sanitize({
@@ -10,11 +10,14 @@ module Klarna
           :family_name    => @family_name,
           :care_of        => @care_of,
           :street_address => @street_address,
+          :street_name    => @street_address,
+          :street_number  => @street_address,
           :postal_code    => @postal_code,
           :city           => @city,
           :country        => @country,
           :email          => @email,
-          :phone          => @phone
+          :phone          => @phone,
+          :title          => @title
         })
       end
     end
